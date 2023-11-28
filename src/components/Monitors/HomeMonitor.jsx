f5d6471be7b02ff4e9d9d53b0e8b09c1fbaa98d4
Daniel Steven Rodriguez Pachon <81037147+danielPachon@users.noreply.github.com>
2023-11-28 17:10:53 -0500

    First Merge

---
+++ src/components/Monitors/HomeMonitor.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function UserLogin(){
+  return(
+    <div>
+      Home monitor
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{hmtl,js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
